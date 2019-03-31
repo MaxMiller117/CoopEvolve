854142db07444982c8dc5aee95f048ba08a9ee12
MaxMiller117 <MaxMiller117@gmail.com>
2019-03-31 18:46:57 -0400

    Added command line inputs and printing output to a file
    Script is now run using python initGenomeGen.py <num_input_nodes> <num_output_nodes> <optional filename>

--- OpenCV Scripts/initGenomeGen.py
+++ OpenCV Scripts/initGenomeGen.py
@@ -1,14 +1,37 @@
+import sys
 
+def write(str):
+    if file == None:
+        print(str)
+    else:
+        file.write(str+"\n")
+
+# python initGenomeGen.py <num_input_nodes> <num_output_nodes> <optional filename>
+N = int(sys.argv[1]) # num_input_nodes
+M = int(sys.argv[2]) # num_output_nodes
+
+file = None
+if(len(sys.argv)>=4):
+    print("Printing to file...")
+    file = sys.argv[3]
+    file = open(file,'w')
 
 # Tool to create initial genome
-print(" genomestart  1")
-for i in range(1,7):
-    print(" node "+str(i)+" 0 1 1")
-for i in range(7,22):
-    print(" node "+str(i)+" 0 0 2")
-for i in range(1,7):
-    for j in range(7,22):
-        print(" gene "+str(i)+" "+str(i)+" "+str(j)+" 0.0 0 "+str(i)+" 0 1")
+write(" genomestart  1")
+
+for i in range(0,N+M+1):
+    write(" trait "+str(i)+" 0."+str(i)+" 0 0 0 0 0 0 0")
+
+for i in range(1,N+1):
+    write(" node "+str(i)+" 0 1 1")
+for i in range(N+1,N+M+1):
+    write(" node "+str(i)+" 0 0 2")
+for i in range(1,N+1):
+    for j in range(N+1,N+M+1):
+        write(" gene "+str(i)+" "+str(i)+" "+str(j)+" 0.0 0 "+str(i)+" 0 1")
 
 
-print(" genomeend 1")
+write(" genomeend 1")
+
+if(not file == None):
+    file.close()
